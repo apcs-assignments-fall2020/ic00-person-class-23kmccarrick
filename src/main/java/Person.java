d5fa8769306330e5a1d947b8a9ebd8cc56ebf6c1
23kmccarrick <23kmccarrick@athenian.org>
2020-10-26 18:26:58 +0000

    opp

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,28 @@
 public class Person {
-    
+    public String firstName;
+    public String lastName;
+    public Person(String a, String b){
+        this.firstName = a;
+        this.lastName = b;
+    }
+    public void printPerson(){
+        Person bruh = new Person("Rishi", "Reddy");
+        System.out.println(bruh);
+    }
+    public void doSomething(){
+        System.out.println("I am taking a nap");
+    }
+    public void setFirstName(String firstName){
+        if(firstName.length()!=0)
+            this.firstName = firstName;
+    }
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+
 }
 
 
